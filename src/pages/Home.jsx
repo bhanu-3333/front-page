e30d0e07a0d505983585a0ebe575abe6f6589da3
b75488460 <b75488460@gmail.com>
2025-03-06 17:31:31 +0000

    home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../pages/Home.css";
+
+const Home = () => {
+  return (
+    <div className="parent">
+      <div className="div1">1</div>
+      <div className="div3">3</div>
+      <div className="div5">5</div>
+      <div className="div6">6</div>
+      <div className="div7">7</div>
+      <div className="div8">8</div>
+    </div>
+  );
+};
+
+export default Home;
